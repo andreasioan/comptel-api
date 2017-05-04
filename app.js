3ac0b3105f43f46e00c1549c725eef74226ee9ad
andreasioan <a.j.ioannidis@hotmail.com>
2017-05-04 15:58:56 +1000

    Update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var mongoose = require('mongoose');
-var socketio = requrie('socketio');
+var socketio = require('socketio');
 
 mongoose.connect('public:swinburne@ds117899.mlab.com:17899/comptel-afom');
 
