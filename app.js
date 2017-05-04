a7a8bd32397874ded4458494e8894a104b071dc1
andreasioan <a.j.ioannidis@hotmail.com>
2017-05-04 16:27:59 +1000

    Update

--- app.js
+++ app.js
@@ -6,25 +6,20 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var mongoose = require('mongoose');
-var io = require('socket.io')();
 
 mongoose.connect('public:swinburne@ds117899.mlab.com:17899/comptel-afom');
 
-var app = express();
-
-// Socket.io
-app.io = io;
-
-console.log('Before io ------------');
-io.on( "connection", function( socket )
-{
-    console.log( "A user connected" );
-});
-console.log('After io ------------');
-
-var index = require('./routes/index')(io);
+var index = require('./routes/index');
 var api = require('./routes/api');
 
+var app = express();
+
+var sockIO = require('socket.io')();
+app.sockIO = sockIO;
+
+sockIO.on('connection', function(socket){
+    console.log('A client connection occurred!');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,51 +30,49 @@ app.set('view engine', 'hbs');
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add headers
 app.use(function (req, res, next) {
 
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-	// Pass to next layer of middleware
-	next();
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use('/', index);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
