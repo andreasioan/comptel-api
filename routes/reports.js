259c850cbd5fd417974ddd07c68a4d3376b9f1db
ANDREAS IOANNIDIS <a.j.ioannidis@hotmail.com>
2017-05-16 16:59:11 +1000

    Update

--- routes/reports.js
+++ routes/reports.js
@@ -126,7 +126,60 @@ router.get('/dates', function (req, res, next) {
     });
 });
 
-router.get('/fallout', function(req, res, next) {
+router.get('/fallout', function (req, res, next) {
+
+    if (req.query.code) {
+        var error = 'ERROR';
+        var num = req.query.code;
+
+        let errorCode0 = Fallout.count()
+            .where('error_code')
+            .gte(error + num)
+            .lte(error + (num + 1));
+
+        let promises = {
+            errorCode0: errorCode0,
+            // falloutsMonth1Count: falloutsMonth1,
+            // falloutsMonth2Count: falloutsMonth2,
+            // falloutsMonth3Count: falloutsMonth3,
+            // falloutsMonth4Count: falloutsMonth4,
+            // falloutsMonth5Count: falloutsMonth5,
+            // falloutsMonth6Count: falloutsMonth6,
+            // resolutionsMonth0Count: resolutionsMonth0,
+            // resolutionsMonth1Count: resolutionsMonth1,
+            // resolutionsMonth2Count: resolutionsMonth2,
+            // resolutionsMonth3Count: resolutionsMonth3,
+            // resolutionsMonth4Count: resolutionsMonth4,
+            // resolutionsMonth5Count: resolutionsMonth5,
+            // resolutionsMonth6Count: resolutionsMonth6
+        };
+
+        promises = Object.keys(promises).map((x) => promises[x]);
+        return Promise.all(promises).then((data) => {
+            let result = {
+                error_code_0: data[0],
+                // fallouts_month_1_count: data[1],
+                // fallouts_month_2_count: data[2],
+                // fallouts_month_3_count: data[3],
+                // fallouts_month_4_count: data[4],
+                // fallouts_month_5_count: data[5],
+                // fallouts_month_6_count: data[6],
+                // resolutions_month_0_count: data[7],
+                // resolutions_month_1_count: data[8],
+                // resolutions_month_2_count: data[9],
+                // resolutions_month_3_count: data[10],
+                // resolutions_month_4_count: data[11],
+                // resolutions_month_5_count: data[12],
+                // resolutions_month_6_count: data[13]
+            };
+
+            return res.status(200).json(result);
+        });
+
+    }
+
+
+
 
 });
 
