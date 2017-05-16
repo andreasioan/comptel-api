f61899ba46cc1786ac5b891f40cdc76fa0aab7b4
ANDREAS IOANNIDIS <a.j.ioannidis@hotmail.com>
2017-05-16 17:38:09 +1000

    Update

--- routes/reports.js
+++ routes/reports.js
@@ -133,51 +133,94 @@ router.get('/fallout', function (req, res, next) {
         var num = req.query.code;
 
         let errorCode0 = Fallout.count()
-            .where('error_code')
-            .gte(error + num)
-            .lte(error + (num + 1));
+            .where('source_error_code')
+            .eq(error + parseInt(num));
+        let errorCode1 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 1));
+        let errorCode2 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 2));
+        let errorCode3 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 3));
+        let errorCode4 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 4));
+        let errorCode5 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 5));
+        let errorCode6 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 6));
+        let errorCode7 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 7));
+        let errorCode8 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 8));
+        let errorCode9 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 9));
+        let errorCode10 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 10));
+        let errorCode11 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 11));
+        let errorCode12 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 12));
+        let errorCode13 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 13));
+        let errorCode14 = Fallout.count()
+            .where('source_error_code')
+            .eq(error + (parseInt(num) + 14));
+
+        console.log(error + (parseInt(num) + 1));
 
         let promises = {
             errorCode0: errorCode0,
-            // falloutsMonth1Count: falloutsMonth1,
-            // falloutsMonth2Count: falloutsMonth2,
-            // falloutsMonth3Count: falloutsMonth3,
-            // falloutsMonth4Count: falloutsMonth4,
-            // falloutsMonth5Count: falloutsMonth5,
-            // falloutsMonth6Count: falloutsMonth6,
-            // resolutionsMonth0Count: resolutionsMonth0,
-            // resolutionsMonth1Count: resolutionsMonth1,
-            // resolutionsMonth2Count: resolutionsMonth2,
-            // resolutionsMonth3Count: resolutionsMonth3,
-            // resolutionsMonth4Count: resolutionsMonth4,
-            // resolutionsMonth5Count: resolutionsMonth5,
-            // resolutionsMonth6Count: resolutionsMonth6
+            errorCode1: errorCode1,
+            errorCode2: errorCode2,
+            errorCode3: errorCode3,
+            errorCode4: errorCode4,
+            errorCode5: errorCode5,
+            errorCode6: errorCode6,
+            errorCode7: errorCode7,
+            errorCode8: errorCode8,
+            errorCode9: errorCode9,
+            errorCode10: errorCode10,
+            errorCode12: errorCode12,
+            errorCode13: errorCode13,
+            errorCode14: errorCode14
         };
 
         promises = Object.keys(promises).map((x) => promises[x]);
         return Promise.all(promises).then((data) => {
             let result = {
                 error_code_0: data[0],
-                // fallouts_month_1_count: data[1],
-                // fallouts_month_2_count: data[2],
-                // fallouts_month_3_count: data[3],
-                // fallouts_month_4_count: data[4],
-                // fallouts_month_5_count: data[5],
-                // fallouts_month_6_count: data[6],
-                // resolutions_month_0_count: data[7],
-                // resolutions_month_1_count: data[8],
-                // resolutions_month_2_count: data[9],
-                // resolutions_month_3_count: data[10],
-                // resolutions_month_4_count: data[11],
-                // resolutions_month_5_count: data[12],
-                // resolutions_month_6_count: data[13]
+                error_code_1: data[1],
+                error_code_2: data[2],
+                error_code_3: data[3],
+                error_code_4: data[4],
+                error_code_5: data[5],
+                error_code_6: data[6],
+                error_code_7: data[7],
+                error_code_8: data[8],
+                error_code_9: data[9],
+                error_code_10: data[10],
+                error_code_11: data[11],
+                error_code_12: data[12],
+                error_code_13: data[13],
+                error_code_14: data[14],
             };
 
             return res.status(200).json(result);
         });
 
     }
-
 
 
 
