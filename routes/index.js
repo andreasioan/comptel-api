7eff2d02823cb367dcb7b8f66ad191e71fdbe1c7
Andreas Ioannidis <a.j.ioannidis@hotmail.com>
2017-06-07 11:37:57 +1000

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/home', function (req, res, next) {
+  res.redirect('/home/index.html');
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
-router.get('/home', function (req, res, next) {
-  res.redirect('/home/index.html');
-})
 
 router.use('/api', require('./api'));
 
